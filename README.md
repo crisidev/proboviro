534cf171786eb7cd345880244210c8d497bafb6b
Matteo Bigoi <bigo@crisidev.org>
2015-09-30 01:15:07 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# proboviro
-An inflexible alerts notifier
+# Proboviro
+An inflexible alerts notifier.
+
+## Info
+Proboviro is a webhook for Prometheus Alertmanager sending alarms as
+push notification to an Android phone using NotifyMyAndroid.
+* [Prometheus](https://github.com/prometheus/prometheus)
+* [Alertmanager](https://github.com/prometheus/alertmanager)
+* [NotifyMyAndroid](https://www.notifymyandroid.com)
+
+It is a webserver which accepts only JSON POST with this structure:
+```json
+{
+   "version": "1",
+   "status": "firing",
+   "alert": [
+      {
+         "summary": "summary",
+         "description": "description",
+         "labels": {
+            "alertname": "TestAlert"
+         },
+         "payload": {
+            "activeSince": "2015-06-01T12:55:47.356+01:00",
+            "alertingRule": "ALERT TestAlert IF absent(metric_name) FOR
+0y WITH ",
+            "generatorURL":
+"http://localhost:9090/graph#%5B%7B%22expr%22%3A%22absent%28metric_name%29%22%2C%22tab%22%3A0%7D%5D",
+            "value": "1"
+         }
+      }
+   ]
+}
+```
+
+To run Proboviro you must specify an API key for NotifyMyAndroid.
---
+++ proboviro.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/dustin/go-nma"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+const (
+	APP_NAME    = "proboviro"
+	APP_VERSION = "0.1"
+	APP_AUTHOR  = "bigo@crisidev.org"
+	APP_SITE    = "https://github.com/crisidev/proboviro"
+)
+
+var (
+	lg         Logger
+	severities = map[string]int{
+		"info": 3,
+		"page": 2,
+	}
+
+	// Flags
+	flagDebug  = kingpin.Flag("debug", "enable debug mode").Short('D').Bool()
+	flagBind   = kingpin.Flag("bind", "bind to address").Short('b').Default(":8082").String()
+	flagApiKey = kingpin.Flag("apikey", "notifymyandroid API key").Short('a').Required().String()
+)
+
+// Simple logging structure
+type Logger struct {
+	output *log.Logger
+	stderr *log.Logger
+}
+
+func init() {
+	kingpin.Version(APP_VERSION)
+	kingpin.Parse()
+	lg.SetupLog()
+}
+
+// Setup debugging to stderr
+func (l Logger) SetupLog() {
+	lg.output = log.New(os.Stdout, "", 0)
+	lg.output.SetFlags(log.Ldate | log.Ltime)
+}
+
+// Logs to stardard output
+func (l Logger) Out(msg string) {
+	if *flagDebug {
+		l.output.Println(msg)
+	}
+}
+
+// Logs to standard output without carriage return
+func (l Logger) OutRaw(msg string) {
+	if *flagDebug {
+		fmt.Printf(msg)
+	}
+}
+
+// Logs a fatal error
+func (l Logger) Fatal(err error) {
+	if err != nil {
+		l.output.Printf(fmt.Sprintf("ERROR: %s", err.Error()))
+	}
+	os.Exit(1)
+}
+
+// Logs an erro
+func (l Logger) Error(err error) {
+	if err != nil {
+		l.output.Printf(fmt.Sprintf("ERROR: %s", err.Error()))
+	}
+}
+
+type SingleAlert struct {
+	Description string            `json:"description"`
+	Summary     string            `json:"summary"`
+	Runbook     string            `json:"runbook"`
+	Labels      map[string]string `json:"labels"`
+	Payload     map[string]string `json:"payload"`
+}
+
+type Alerts struct {
+	Alert   []SingleAlert `json:"alert"`
+	Status  string        `json:"status"`
+	Version string        `json:"version"`
+}
+
+func DoAlert(rw http.ResponseWriter, req *http.Request, notifier *nma.NMA) {
+	alerts, err := DecodeJson(req.Body)
+	if err != nil {
+		lg.Error(err)
+	}
+	for _, alert := range alerts.Alert {
+		go HandleAlert(alert, notifier)
+	}
+}
+
+func DecodeJson(body io.ReadCloser) (alerts Alerts, err error) {
+	decoder := json.NewDecoder(body)
+	err = decoder.Decode(&alerts)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func HandleAlert(alert SingleAlert, notifier *nma.NMA) {
+	severity := alert.Labels["severity"]
+	if severity != "" {
+		lg.Out(fmt.Sprintf("got new alert with severity: %s. send to notifymyandroid", severity))
+		if severities[severity] == 0 {
+			severity = "info"
+		}
+		go Page(alert, severities[severity], notifier)
+	} else {
+		lg.Error(errors.New("\"severity\" tag not found. this alert is not for me"))
+	}
+}
+
+func Page(alert SingleAlert, severity int, notifier *nma.NMA) {
+	notification := nma.Notification{
+		Application: fmt.Sprintf("%s:%s", APP_NAME, alert.Labels["alertname"]),
+		Description: alert.Description,
+		Event:       alert.Summary,
+		Priority:    1,
+		URL:         alert.Payload["generatorURL"],
+	}
+	if err := notifier.Notify(&notification); err != nil {
+		lg.Error(err)
+	}
+}
+
+func main() {
+	notifier := nma.New(*flagApiKey)
+	http.HandleFunc("/irc", func(w http.ResponseWriter, req *http.Request) {
+		DoAlert(w, req, notifier)
+	})
+	err := http.ListenAndServe(*flagBind, nil)
+	if err != nil {
+		lg.Fatal(err)
+	}
+	os.Exit(0)
+}
